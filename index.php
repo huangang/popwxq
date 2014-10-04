661f482d237cf945c72d04ca7a7e3e0d06ce487f
zhanghuangang <huangangzhang@gmail.com>
2014-10-04 15:21:33 +0800

    缺少index.php。。。＝＝补上

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+	//1.确定应用名称 App
+	define('APP_NAME','Home');
+	//2.确定应用路径
+	define('APP_PATH','./Home/');
+	//3.开启调试模式
+	define('APP_DEBUG',true);
+	//4.应用核心文件
+	require './ThinkPHP/ThinkPHP.php';
+?>
