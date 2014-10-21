a3f9631413fcba62ff834cd1ce43bf1f498a57fd
zhanghuangang <huangangzhang@gmail.com>
2014-10-21 14:48:53 +0800

    支持表情上墙

--- Home/Lib/Action/WapAction.class.php
+++ Home/Lib/Action/WapAction.class.php
@@ -15,8 +15,8 @@ class WapAction extends Action {
 		}
 		
 	}
-	public function wallmessage(){
 	
+	public function wallmessage(){
 		$wall=M('wall')->find();
 		if($wall[examine]=='1'){
 			$where['examine']="1";	
@@ -32,7 +32,114 @@ class WapAction extends Action {
 		$data[$lastid]['id'] = $info[0]['Id'];
 		$data[$lastid]['fakeid'] = $info[0]['fake_id'];
 		$data[$lastid]['num'] = $lastid;
-		$data[$lastid]['content'] = $info[0]['content'];
+		//表情比对
+		$content = $info[0]['content'];
+		if(strstr($content,"/:")){
+		      $table_change = array("/::)"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/0.gif' width=25px hight=25px />",
+			  "/::~"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/1.gif' width=25px hight=25px />",
+			  "/::B"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/2.gif' width=25px hight=25px />",
+			  "/::|"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/3.gif' width=25px hight=25px />",
+			  "/:8-)"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/4.gif' width=25px hight=25px />",
+			  "/::<"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/5.gif' width=25px hight=25px />",
+			  "/::$"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/6.gif' width=25px hight=25px />",
+			  "/::X"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/7.gif' width=25px hight=25px />",
+			  "/::Z"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/8.gif' width=25px hight=25px />",
+			  "/::xb("=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/9.gif' width=25px hight=25px />",
+			  "/::-|"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/10.gif' width=25px hight=25px />",
+			  "/::@"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/11.gif' width=25px hight=25px />",
+			  "/::P"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/12.gif' width=25px hight=25px />",
+			  "/::D"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/13.gif' width=25px hight=25px />",
+			  "/::O"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/14.gif' width=25px hight=25px />",
+			  "/::("=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/15.gif' width=25px hight=25px />",
+			  "/::+"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/16.gif' width=25px hight=25px />",
+			  "/:--b"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/17.gif' width=25px hight=25px />",
+			  "/::Q"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/18.gif' width=25px hight=25px />",
+			  "/::T"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/19.gif' width=25px hight=25px />",
+			  "/:,@P"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/20.gif' width=25px hight=25px />",
+			  "/:,@-D"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/21.gif' width=25px hight=25px />",
+			  "/::d"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/22.gif' width=25px hight=25px />",
+			  "/:,@o"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/23.gif' width=25px hight=25px />",
+			  "/::g"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/24.gif' width=25px hight=25px />",
+			  "/:|-)"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/25.gif' width=25px hight=25px />",
+			  "/::!"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/26.gif' width=25px hight=25px />",
+			  "/::L"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/27.gif' width=25px hight=25px />",
+			  "/::>"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/28.gif' width=25px hight=25px />",
+			  "/::,@"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/29.gif' width=25px hight=25px />",
+			  "/:,@f"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/30.gif' width=25px hight=25px />",
+			  "/::-S"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/31.gif' width=25px hight=25px />",
+			  "/:?"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/32.gif' width=25px hight=25px />",
+			  "/:,@x"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/33.gif' width=25px hight=25px />",
+			  "/:,@@"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/34.gif' width=25px hight=25px />",
+			  "/::8"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/35.gif' width=25px hight=25px />",
+			  "/:,@!"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/36.gif' width=25px hight=25px />",
+			  "/:!!!"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/37.gif' width=25px hight=25px />",
+			  "/:xx"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/38.gif' width=25px hight=25px />",
+			  "/:bye"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/39.gif' width=25px hight=25px />",
+			  "/:wipe"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/40.gif' width=25px hight=25px />",
+			  "/:dig"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/41.gif' width=25px hight=25px />",
+			  "/:handclap"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/42.gif' width=25px hight=25px />",
+			  "/:&-("=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/43.gif' width=25px hight=25px />",
+			  "/:B-)"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/44.gif' width=25px hight=25px />",
+			  "/:<@"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/45.gif' width=25px hight=25px />",
+			  "/:@>"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/46.gif' width=25px hight=25px />",
+			  "/::-O"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/47.gif' width=25px hight=25px />",
+			  "/:>-|"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/48.gif' width=25px hight=25px />",
+			  "/:P-("=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/49.gif' width=25px hight=25px />",
+			  "/::xb|"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/50.gif' width=25px hight=25px />",
+			  "/:X-)"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/51.gif' width=25px hight=25px />",
+			  "/::*"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/52.gif' width=25px hight=25px />",
+			  "/:@x"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/53.gif' width=25px hight=25px />",
+			  "/:8*"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/54.gif' width=25px hight=25px />",
+			  "/:pd"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/55.gif' width=25px hight=25px />",
+			  //注意此行
+			  			"/:"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/56.gif' width=25px hight=25px />",
+			  "/:beer"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/57.gif' width=25px hight=25px />",
+			  "/:basketb"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/58.gif' width=25px hight=25px />",
+			  "/:oo"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/59.gif' width=25px hight=25px />",
+			  "/:coffee"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/60.gif' width=25px hight=25px />",
+			  "/:eat"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/61.gif' width=25px hight=25px />",
+			  "/:pig"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/62.gif' width=25px hight=25px />",
+			  "/:rose"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/63.gif' width=25px hight=25px />",
+			  "/:fade"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/64.gif' width=25px hight=25px />",
+			  "/:showlove"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/65.gif' width=25px hight=25px />",
+			  "/:heart"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/66.gif' width=25px hight=25px />",
+			  "/:break"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/67.gif' width=25px hight=25px />",
+			  "/:cake"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/68.gif' width=25px hight=25px />",
+			  "/:li"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/69.gif' width=25px hight=25px />",
+			  "/:bome"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/70.gif' width=25px hight=25px />",
+			  "/:kn"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/71.gif' width=25px hight=25px />",
+			  "/:footb"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/72.gif' width=25px hight=25px />",
+			  "/:ladybug"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/73.gif' width=25px hight=25px />",
+			  "/:shit"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/74.gif' width=25px hight=25px />",
+			  "/:moon"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/75.gif' width=25px hight=25px />",
+			  "/:sun"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/76.gif' width=25px hight=25px />",
+			  "/:gift"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/77.gif' width=25px hight=25px />",
+			  "/:hug"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/78.gif' width=25px hight=25px />",
+			  "/:strong"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/79.gif' width=25px hight=25px />",
+			  "/:weak"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/80.gif' width=25px hight=25px />",
+			  "/:share"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/81.gif' width=25px hight=25px />",
+			  "/:v"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/82.gif' width=25px hight=25px />",
+			  "/:@)"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/83.gif' width=25px hight=25px />",
+			  "/:jj"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/84.gif' width=25px hight=25px />",
+			  "/:@@"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/85.gif' width=25px hight=25px />",
+			  "/:bad"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/86.gif' width=25px hight=25px />",
+			  "/:lvu"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/87.gif' width=25px hight=25px />",
+			  "/:no"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/88.gif' width=25px hight=25px />",
+			  "/:ok"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/89.gif' width=25px hight=25px />",
+			  "/:love"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/90.gif' width=25px hight=25px />",
+			  //"/:weak"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/91.gif' width=25px hight=25px />",
+			  "/:jump"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/92.gif' width=25px hight=25px />",
+			  "/:shake"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/93.gif' width=25px hight=25px />",
+			  //"/:weak"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/94.gif' width=25px hight=25px />",
+			  "/:circle"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/95.gif' width=25px hight=25px />",
+			  "/:kotow"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/96.gif' width=25px hight=25px />",
+			  "/:turn"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/97.gif' width=25px hight=25px />",
+			  "/:skip"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/98.gif' width=25px hight=25px />",
+			  "/:oY"=>"<img src='http://rescdn.qqmail.com/zh_CN/images/mo/DEFAULT2/99.gif' width=25px hight=25px />"
+	      );
+        $content= strtr($content,$table_change); 
+    }
+		$data[$lastid]['content'] = $content;
 		$data[$lastid]['nickname'] = $info[0]['nickname'];
 		$data[$lastid]['avatar'] = $info[0]['img'];
 		if(count($info)==0){
